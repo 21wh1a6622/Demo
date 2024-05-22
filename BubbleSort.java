7878a045f764a16de6688e3df9648d61144f6382
Neeli Manisha <96616329+21wh1a6622@users.noreply.github.com>
2024-05-22 11:05:02 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+     class BubbleSort {  
+       public static void bubbleSort(int[] arr) {  
+            int n = arr.length;  
+            int temp = 0;  
+             for(int i=0; i < n; i++){  
+                     for(int j=1; j < (n-i); j++){  
+                              if(arr[j-1] > arr[j]){  
+                                     
+                                     temp = arr[j-1];  
+                                     arr[j-1] = arr[j];  
+                                     arr[j] = temp;  
+                             }  
+                              
+                     }  
+             }  
+      
+        }  
+        public static void main(String[] args) {  
+                    int arr[] ={4,5,6,-1,0,7,8,2};  
+                     
+                    System.out.println("Before Sorting");  
+                    for(int i=0; i < arr.length; i++){  
+                            System.out.print(arr[i] + " ");  
+                    }  
+                    System.out.println();  
+                      
+                    bubbleSort(arr);
+                    System.out.println("After Sorting");  
+                    for(int i=0; i < arr.length; i++){  
+                            System.out.print(arr[i] + " ");  
+                    }  
+       
+            }  
+    }  
